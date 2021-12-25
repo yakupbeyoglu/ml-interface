f16524348f5e7ff165f81d0c82cf95d7c4f92040
Yakup <yakupbeyoglu@gmail.com>
2021-12-26 01:53:52 +0200

    Gausian NB model interface class implementation

--- ML/GausianNaiveBayes.py
+++ ML/GausianNaiveBayes.py
@@ -1,11 +1,12 @@
 from ML.DataSet import DataSet
+from ML.MLModel import MLModel
 from sklearn.model_selection import train_test_split
 from sklearn.naive_bayes import GaussianNB
 from sklearn import metrics
 from sklearn.metrics import confusion_matrix
 from sklearn.metrics import f1_score
 
-class GausianNaiveBayes :
+class GausianNaiveBayes(MLModel) :
     def __init__(self, dataset) :
         self.dataset = dataset
         X_train, X_test, y_train, y_test = train_test_split(dataset.GetXData(), dataset.GetYData(), test_size=0.5, random_state=0)
@@ -15,8 +16,42 @@ class GausianNaiveBayes :
         print("Accuracy:",metrics.accuracy_score(y_test, y_pred))
         print("matrix : ", confusion_matrix(y_test, y_pred))
         print("f1 score = ", f1_score(y_test, y_pred, zero_division=1))
+
     
+    def Process(self, test_size = 0.5, random_state = 0) :
+        X_train, X_test, y_train, y_test = train_test_split(self.dataset.GetXData(), self.dataset.GetYData(), test_size = test_size, random_state = random_state)
+        return self.__ProcessAlgorithm(X_train, y_train, X_test)
+
+    def Process(self, x_train, y_train, x_test) :
+        return self.__ProcessAlgorithm(x_train, y_train, x_test)
+    
+    def KFold(self, number) :
+        # To do
+        print("K Fold")
+
+    def GetConfusionMatrix(self, y_test, y_pred):
+        return confusion_matrix(y_test, y_pred)
+    
+    def GetF1Score(self, y_test, y_pred):
+        return f1_score(y_test, y_pred, zero_division = 1)
+    
+    def GetAccurancy(self, y_test, y_pred):
+        return metrics.accuracy_score(y_test, y_pred)
+
+    def GetRecall(self, y_test, y_pred):
+        return metrics.recall_score(y_test, y_pred)
+    
+    def GetPrecision(self, y_test, y_pred):
+        return metrics.precision_score(y_test, y_pred)
+    
+    def GetF1Score(self, y_test, y_pred):
+        return super().GetF1Score(y_test, y_pred)
+
     def GetPriorProbability(self) :
         return self.dataset.GetAmountOfClasses()
 
     
+    def __ProcessAlgorithm(self, x_train, y_train, x_test) : 
+        gnb = GaussianNB()
+        ml_process = gnb.fit(x_train, y_train)
+        return ml_process.predict(x_test)
--- ML/MLModel.py
+++ ML/MLModel.py
@@ -1,40 +1,43 @@
 from abc import ABC, abstractmethod
 
-class MLModel(ABC) :
+class MLModel(ABC):
+
     @abstractmethod
-    # Process Model
-    def Process(self, x_train, y_train, x_test) :
+    # Process Model with inital dataset, return y_prediction
+    def Process(self, test_size=0.5, random_state=0):
+        pass
+
+    @abstractmethod
+    # Process Model with given x_train, y_train, x_test, return y_prediction
+    def Process(self, x_train, y_train, x_test):
         pass
 
     @abstractmethod
     # Fold dataset with given number
-    def KFold(self,  number) : 
+    def KFold(self,  number):
         pass
 
     @abstractmethod
     # Return confusion matrix of the predicted dataset, it can use for recall, f1-score etc calculation
-    def GetConfusionMatrix(self) : 
+    def GetConfusionMatrix(self, y_test, y_pred):
         pass
 
     @abstractmethod
     # Get F1 score with given test and prediction data
-    def GetF1Score(self, y_test, y_pred) :
+    def GetF1Score(self, y_test, y_pred):
         pass
 
     @abstractmethod
     # Get accurancy of the trained model
-    def GetAccurancy(self, y_test, y_pred) : 
+    def GetAccurancy(self, y_test, y_pred):
         pass
-    
+
     @abstractmethod
     # Get Recall value of the trained model
-    def GetRecall(self, y_test, y_pred) :
+    def GetRecall(self, y_test, y_pred):
         pass
 
     @abstractmethod
     # Get Precesion value of trhe trained model
-    def GetPrecision(self, y_test, y_pred) : 
+    def GetPrecision(self, y_test, y_pred):
         pass
-
-
-    
