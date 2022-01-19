22d4add5e44f159e45dee58700f6184d5999f1bc
Yakup <yakupbeyoglu@gmail.com>
2022-01-20 01:32:34 +0200

    Losses types added as TODO

---
+++ ML/Enums/Losses/Hinge.py
@@ -0,0 +1,7 @@
+from ..BaseEnum import BaseEnum
+
+
+class Hinge(BaseEnum):
+    # To Do : regression losses should implement
+    def ToDo():
+        print("Hinge losses should implement, usefullfor maximum margin classification")
---
+++ ML/Enums/Losses/Regression.py
@@ -0,0 +1,8 @@
+from ..BaseEnum import BaseEnum
+
+
+class Regression(BaseEnum):
+    # To Do : regression losses should implement
+    def ToDo():
+        print("Regression losses should implement")
+        
