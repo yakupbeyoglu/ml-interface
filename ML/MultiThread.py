845d7c19b9b8d60eac462bf2b4f8012dc164dadf
yakupbeyoglu <yakupbeyoglu@gmail.com>
2022-01-30 14:40:13 +0200

    Multi thread class added

---
+++ ML/MultiThread.py
@@ -0,0 +1,17 @@
+# Mutli threading with retury type from 
+# https://stackoverflow.com/questions/6893968/how-to-get-the-return-value-from-a-thread-in-python
+from threading import Thread
+
+class MultiThread(Thread):
+    def __init__(self, group=None, target=None, name=None,
+                 args=(), kwargs={}, Verbose=None):
+        Thread.__init__(self, group, target, name, args, kwargs)
+        self._return = None
+
+    def Start(self):
+        if self._target is not None:
+            self._return = self._target(*self._args,
+                                                **self._kwargs)
+    def Join(self, *args):
+        Thread.join(self, *args)
+        return self._return
